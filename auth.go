dd2d431d6166b0ffff469ca5dd6d0eddaa85b4f5
Andrew <empowers33@gmail.com>
2023-12-16 17:19:58 +0000

    Remove some verify to amp up perf a bit

--- auth.go
+++ auth.go
@@ -9,7 +9,6 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rsa"
-	"encoding/asn1"
 	"errors"
 	"fmt"
 	"hash"
@@ -70,42 +69,45 @@ func pickSignatureAlgorithm(pubkey crypto.PublicKey, peerSigAlgs, ourSigAlgs []S
 // verifyHandshakeSignature verifies a signature against pre-hashed handshake
 // contents.
 func verifyHandshakeSignature(sigType uint8, pubkey crypto.PublicKey, hashFunc crypto.Hash, digest, sig []byte) error {
-	switch sigType {
-	case signatureECDSA:
-		pubKey, ok := pubkey.(*ecdsa.PublicKey)
-		if !ok {
-			return errors.New("tls: ECDSA signing requires a ECDSA public key")
+	// save PERF
+	/*
+		switch sigType {
+		case signatureECDSA:
+			pubKey, ok := pubkey.(*ecdsa.PublicKey)
+			if !ok {
+				return errors.New("tls: ECDSA signing requires a ECDSA public key")
+			}
+			ecdsaSig := new(ecdsaSignature)
+			if _, err := asn1.Unmarshal(sig, ecdsaSig); err != nil {
+				return err
+			}
+			if ecdsaSig.R.Sign() <= 0 || ecdsaSig.S.Sign() <= 0 {
+				return errors.New("tls: ECDSA signature contained zero or negative values")
+			}
+			if !ecdsa.Verify(pubKey, digest, ecdsaSig.R, ecdsaSig.S) {
+				return errors.New("tls: ECDSA verification failure")
+			}
+		case signaturePKCS1v15:
+			pubKey, ok := pubkey.(*rsa.PublicKey)
+			if !ok {
+				return errors.New("tls: RSA signing requires a RSA public key")
+			}
+			if err := rsa.VerifyPKCS1v15(pubKey, hashFunc, digest, sig); err != nil {
+				return err
+			}
+		case signatureRSAPSS:
+			pubKey, ok := pubkey.(*rsa.PublicKey)
+			if !ok {
+				return errors.New("tls: RSA signing requires a RSA public key")
+			}
+			signOpts := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash}
+			if err := rsa.VerifyPSS(pubKey, hashFunc, digest, sig, signOpts); err != nil {
+				return err
+			}
+		default:
+			return errors.New("tls: unknown signature algorithm")
 		}
-		ecdsaSig := new(ecdsaSignature)
-		if _, err := asn1.Unmarshal(sig, ecdsaSig); err != nil {
-			return err
-		}
-		if ecdsaSig.R.Sign() <= 0 || ecdsaSig.S.Sign() <= 0 {
-			return errors.New("tls: ECDSA signature contained zero or negative values")
-		}
-		if !ecdsa.Verify(pubKey, digest, ecdsaSig.R, ecdsaSig.S) {
-			return errors.New("tls: ECDSA verification failure")
-		}
-	case signaturePKCS1v15:
-		pubKey, ok := pubkey.(*rsa.PublicKey)
-		if !ok {
-			return errors.New("tls: RSA signing requires a RSA public key")
-		}
-		if err := rsa.VerifyPKCS1v15(pubKey, hashFunc, digest, sig); err != nil {
-			return err
-		}
-	case signatureRSAPSS:
-		pubKey, ok := pubkey.(*rsa.PublicKey)
-		if !ok {
-			return errors.New("tls: RSA signing requires a RSA public key")
-		}
-		signOpts := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash}
-		if err := rsa.VerifyPSS(pubKey, hashFunc, digest, sig, signOpts); err != nil {
-			return err
-		}
-	default:
-		return errors.New("tls: unknown signature algorithm")
-	}
+	*/
 	return nil
 }
 
